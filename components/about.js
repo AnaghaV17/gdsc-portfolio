ac8c66a3469760dd57b67e16916947e419022d78
Anagha V <anagha.7v@gmail.com>
2022-11-26 07:38:23 +0000

    Update 3 files

--- components/about.js
+++ components/about.js
@@ -12,8 +12,22 @@ function About() {
             className='about px-7 py-20 flex flex-col items-center justify-center max-w-7xl mx-auto'
             id='about'>
             <h2 className='about-heading flex items-center w-full mb-20 whitespace-nowrap leading-[1.1] font-semibold text-slate-300 text-step-5'>
-                About Me
+                About<span className="text-green-400"> Me</span>
             </h2>
+            
+            <div className="content_container" flex gap-12>
+            <div className="content_left">
+                <div className="content_abotme">
+                    <p className="mt-4">
+                        shgshjadgaghjahahs
+                    </p>
+                    </div>
+                    <div className="content_technologies"></div>
+                   
+                    </div>
+                    <div className="content_right"></div>
+                    <img src="/rick.gif" className="w-1/4"/>
+                </div>
         </div>
     );
 }
--- components/projects.js
+++ components/projects.js
@@ -6,8 +6,31 @@ function Projects(props) {
             <h2 className='mb-20 whitespace-nowrap leading-[1.1] font-semibold text-slate-300 text-step-5'>
                 Projects
             </h2>
+            <div className="bg-slate-800 p-4 rounded-sm">
+        <p className="text-yellow-400 font-bold text-sm">Featured project</p>
+        <p>Portfolio</p>
+        <p>hdgegyetyrbfghbhdj</p>
+        <button>click me</button>
+        <a href="https://github.com/AnaghaV17/gdsc-portfolio" className="underline ml-4 hover:">My github</a>
+        
+        </div>
         </div>
     );
 }
 
+function ProjectCard() {
+    return(<div className="bg-slate-800 p-4 rounded-sm">
+        <p className="text-yellow-400 font-bold text-sm">Featured project</p>
+        <p>Portfolio</p>
+        <p>hdgegyetyrbfghbhdj</p>
+        <button>click me</button>
+        <a href="https://github.com/AnaghaV17/gdsc-portfolio" className="underline ml-4">My github</a>
+        
+        </div>);}
+
+
+
 export default Projects;
+
+
+  
